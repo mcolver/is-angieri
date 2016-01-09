0c772d5556ac84dbe0c80bae69335ea6132b4f24
isangieri <isabelleangieri@gmail.com>
2016-01-09 13:53:34 -0800

    setup mongolab

--- server.js
+++ server.js
@@ -47,3 +47,10 @@ var db = require("./models/index.js");
 app.listen(process.env.PORT || 3000, function () {
   console.log('Express server is running on http://localhost:3000/');
 });
+
+//mongoose connect
+mongoose.connect(
+  process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/is-angieri'
+);
