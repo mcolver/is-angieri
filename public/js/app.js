8ba30347b53d2171d6071e481ac036b6e1cad2cd
isangieri <isabelleangieri@gmail.com>
2016-02-07 09:51:53 -0800

    refactored styling and content

--- public/js/app.js
+++ public/js/app.js
@@ -8,33 +8,33 @@
 $(document).ready(function() {
   console.log('app.js loaded!');
 
-  $("document").ready(function() {
+  // $("document").ready(function() {
 
-    // Creates canvas 320 × 200 at 10, 50
-    var paper = Raphael(10, 50, 320, 200);
+  //   // Creates canvas 320 × 200 at 10, 50
+  //   var paper = Raphael(10, 50, 320, 200);
 
-    // Creates circle at x = 50, y = 40, with radius 10
-    var circle = paper.circle(50, 40, 10);
-    // Sets the fill attribute of the circle to red (#f00)
-    circle.attr("fill", "#EEEEEE");
+  //   // Creates circle at x = 50, y = 40, with radius 10
+  //   var circle = paper.circle(50, 40, 10);
+  //   // Sets the fill attribute of the circle to red (#f00)
+  //   circle.attr("fill", "#EEEEEE");
 
-    // Sets the stroke attribute of the circle to white
-    circle.attr("stroke", "#EEEEEE");
+  //   // Sets the stroke attribute of the circle to white
+  //   circle.attr("stroke", "#EEEEEE");
 
-    $("path").hover(function(){
-      $(this).css({ stroke: "#EA730B"});
-      $(this).css({ stroke: "#000"});
-    });
+  //   $("path").hover(function(){
+  //     $(this).css({ fill: "#ffffff"});
+  //     $(this).css({ stroke: "#000"});
+  //   });
 
-    // css({ fill: "#ff0000" });
+  //   // css({ fill: "#ff0000" });
 
-    // $("path").mouseenter(function(){
-    //   $(this).animate({ stroke: "#EA730B"});
-    // });
+  //   // $("path").mouseenter(function(){
+  //   //   $(this).animate({ stroke: "#EA730B"});
+  //   // });
 
-    // <path fill-rule="evenodd" clip-rule="evenodd" fill="#597C48" stroke="#FF7300" stroke-width="2">
+  //   // <path fill-rule="evenodd" clip-rule="evenodd" fill="#597C48" stroke="#FF7300" stroke-width="2">
 
-  });
+  // });
 
 
 });
