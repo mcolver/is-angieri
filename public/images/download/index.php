4d4f9508af48a24a1df8b536c5c5f35a78b7ff94
isangieri <isabelleangieri@gmail.com>
2016-02-10 10:46:12 -0800

    added new project information and github links

---
+++ public/images/download/index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="description" content="A beautiful HTML & CSS built iPhone 6 mock for your website." />
+<title>MocKit - Beautiful HTML & CSS built iPhone 6 mock</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="shortcut icon" href="../favicon.png">
+
+</head>
+
+<body>
+
+    <div class="half dark">
+        <div class="iphone-container">
+            <div class="phone-shape small">
+                <span class="button-one buttons"></span>
+                <span class="button-two buttons"></span>
+                <span class="button-three buttons"></span>
+                <span class="button-four button last"></span>
+                <div class="top-details">
+                    <span class="camera"></span>
+                    <span class="circle"></span>
+                    <span class="speaker"></span>
+                </div>
+                <div class="phone-screen">
+                    <!-- Put your screenshot here <img src="yourscreen.png" height="100%" width="100%" />-->
+                </div>
+                <div class="circle-button"></div>
+            </div>
+        </div>
+	</div>
+
+    <div class="half light">
+        <div class="iphone-container">
+            <div class="phone-shape small">
+                <span class="button-one buttons"></span>
+                <span class="button-two buttons"></span>
+                <span class="button-three buttons"></span>
+                <span class="button-four button last"></span>
+                <div class="top-details">
+                    <span class="camera"></span>
+                    <span class="circle"></span>
+                    <span class="speaker"></span>
+                </div>
+                <div class="phone-screen">
+                    <!-- Put your screenshot here <img src="yourscreen.png" height="100%" width="100%" />-->
+                </div>
+                <div class="circle-button"></div>
+            </div>
+        </div>
+	</div>
+        
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript" src="scripts.js"></script>
+</body>
+</html>
